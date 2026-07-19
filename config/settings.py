6b3a5b51149e3dc7a2667759acc4d90210cd8210
AfaQueSoomro <102575344+AfaQueSoomro@users.noreply.github.com>
2026-07-20 00:13:19 +0500

    intilize commit

--- config/settings.py
+++ config/settings.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
+# BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Use environment variable or fallback to default
